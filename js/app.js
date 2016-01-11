5664820af726a384c63ddb4a225960029431885f
ele0 <ele0@users.noreply.github.com>
2016-01-11 15:41:37 -0500

    Feat: Update Enemy class with basic movements

--- js/app.js
+++ js/app.js
@@ -6,6 +6,13 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    // Setting the Enemy initial location
+    this.x = getRandomIntInclusive(1,20) * -101;
+    this.y = (getRandomIntInclusive(1,3) * 83) - 20;
+    // Setting the Enemy speed.
+    // TODO: tweak actual edge values after testing.
+    this.speed = getRandomIntInclusive(15,500);
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +21,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // TODO: Updates the Enemy location (you need to implement)
+    this.x += this.speed * dt;
+
+    // TODO: Handles collision with the Player
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +36,27 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+
+    // Setting the Players initial location
+    this.x = 2 * 101;
+    this.y = 5 * 171;
+
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var numEnemies = 10;
+var allEnemies = [];
 
+for (i=0;i<numEnemies;i++){
+    allEnemies[i] = new Enemy();
+};
 
 
 // This listens for key presses and sends the keys to your
@@ -44,3 +71,10 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+// From MDN example
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
